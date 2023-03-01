e5f72c81caabe4d9545570ada52208a91430f400
Arpan Bhavsar <arpanbhavsar.160410116019@gmail.com>
2023-03-01 05:49:49 -0800

    Added a static path for flutter web.

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ require('dotenv').config()
 const path = require('path')
 const routes = require('./routes')
 
+const express = require('express');
+
+
 // Import the LTI provider module
 const lti = require('ltijs').Provider
+
 
 // Setup the LTI provider with the LTI key, MongoDB URL and options
 lti.setup(process.env.LTI_KEY,
   {
     url: 'mongodb+srv://admin:PeaMTZuCL4UZSPEy@ilearncluster.3tc9lwg.mongodb.net/?retryWrites=true&w=majority'
   }, {
+    staticPath: path.join(__dirname, './public'), // Path to static files
     cookies: {
       secure: true, // Set secure to true if the testing platform is in a different domain and https is being used
       sameSite: 'None' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
@@ -23,11 +28,20 @@ lti.setup(process.env.LTI_KEY,
 // This will handle successful LTI launches and redirect to the app
 lti.onConnect(async (token, req, res) => {
   console.log('LTI connection successful.');
-  return res.send('LTI connection successful.');
+  console.log(res.locals.ltik);
+  process.env.LTIK=res.locals.ltik;
+  res.set({
+    Authorization : 'Bearer ' + res.locals.ltik
+  });
+  // return res.send('LTI connection successful.');
+  // console.log(lti.app.use(express.static(
+  //   path.join(__dirname, './views'))));
+  return res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
 // Setting up routes
 // lti.app.use(routes)
+// lti.app.use(express.static('views', { extensions: ['html', 'js'] }));
 // Define the setup function
 const setup = async () => {
   // Deploy the LTI provider on the specified port
@@ -46,9 +60,13 @@ const setup = async () => {
   // Get the platform authentication configuration
   const authConfig = await platform.platformAuthConfig()
   console.log(authConfig)
-lti.app.use(routes)
+lti.app.use(routes);
+// lti.app.use(express.static('views', { extensions: ['html', 'js'] }));
+
 
 }
+
+
 
 // Call the setup function
 setup()
--- routes.js
+++ routes.js
@@ -2,5 +2,12 @@ const router = require("express").Router();
 // Requiring Ltijs
 const lti = require("ltijs").Provider;
 
+router.get('/flutter.js', (req, res) => {
+        console.log('LTIK from route: '+process.env.LTIK);
+        return res.send('Went to flutter.js.');
+        // return res.sendFile(path.join(__dirname, './views/index.html'));
+});
 
+// Wildcard route to deal with redirecting to React routes
+router.get('*', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
 module.exports = router;
