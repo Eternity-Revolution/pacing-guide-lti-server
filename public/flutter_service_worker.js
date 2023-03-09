d26090abb91020cf360d12e0338c287f6e8b393a
Arpan Bhavsar <arpanbhavsar.160410116019@gmail.com>
2023-03-09 15:17:05 -0800

    Modified to check if all dates are showing or not.

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "bca3ecfcbeb720e0519f8a9f30271ec7",
-"index.html": "8c8fce53eb59ff726f036de558239936",
-"/": "8c8fce53eb59ff726f036de558239936",
-"main.dart.js": "36af5a1145f220d5ffbf05b79f3b2912",
+"index.html": "2e08e3cc2d6faabb21b73df1dbffbc16",
+"/": "2e08e3cc2d6faabb21b73df1dbffbc16",
+"main.dart.js": "d9f9be543093a095d8ea94b5193a7b39",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
