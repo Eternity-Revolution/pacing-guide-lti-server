8836e1bd4c0f0170e858c7ac4fda531bfe245fd8
Arpan Bhavsar <arpanbhavsar.160410116019@gmail.com>
2023-03-09 19:26:24 -0800

    Student side try.

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "bca3ecfcbeb720e0519f8a9f30271ec7",
-"index.html": "6b95f5484919250d4e5d48c90c41e399",
-"/": "6b95f5484919250d4e5d48c90c41e399",
-"main.dart.js": "5ead14a576e0744fe2d62525b2fc1684",
+"index.html": "4faded4a8c98938adbd5fb8c4bf40bb3",
+"/": "4faded4a8c98938adbd5fb8c4bf40bb3",
+"main.dart.js": "7a44ac8efc33ade026126fc4d8806ba3",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
