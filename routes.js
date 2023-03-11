b4a25dc5236d80d7c220a6fa78cf7468bfa2c915
Arpan Bhavsar <arpanbhavsar.160410116019@gmail.com>
2023-03-10 17:38:57 -0800

    Printing req keys.

--- routes.js
+++ routes.js
@@ -125,7 +125,8 @@ router.get("/info", async (req, res) => {
   if (context.roles) info.roles = context.roles;
   if (context.context) info.context = context.context;
   // info.res=JSON.stringify(res);
-  console.log(Object.keys(res.req));
+  console.log(Object.keys(res.outputData));
+  console.log(Object.keys(req));
 //   if(JSON.stringify(res).includes('138932397@iLearn.com')){
 // console.log('Username found.');
 //   }
