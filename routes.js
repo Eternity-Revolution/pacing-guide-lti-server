a192f485c2bc7b355fb9f91794a79e50814a45af
Arpan Bhavsar <arpanbhavsar.160410116019@gmail.com>
2023-03-10 17:34:11 -0800

    Print res body.

--- routes.js
+++ routes.js
@@ -125,10 +125,10 @@ router.get("/info", async (req, res) => {
   if (context.roles) info.roles = context.roles;
   if (context.context) info.context = context.context;
   // info.res=JSON.stringify(res);
-  console.log(res.locals);
-  if(JSON.stringify(res).includes('138932397@iLearn.com')){
-console.log('Username found.');
-  }
+  console.log(res.body);
+//   if(JSON.stringify(res).includes('138932397@iLearn.com')){
+// console.log('Username found.');
+//   }
   return res.send(info);
 });
 
